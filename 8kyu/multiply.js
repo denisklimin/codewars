2b2e7b56c95a34169f5319b0cd1581dfbf4223d5
denisklimin <denisbusinessman1995@gmail.com>
2024-07-28 14:56:40 +0200

    init

---
+++ 8kyu/multiply.js
@@ -0,0 +1,12 @@
+// This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+    let c = 0;
+    c = a * b;
+    return c;
+}
+
+  // const num1 = +promt('First');
+  // const num2 = +promt('Second');
+
+multiply(1, 1);
