25264714d2eecbedd54a5307191c069d90258274
denisklimin <denisbusinessman1995@gmail.com>
2024-07-30 17:24:03 +0200

    3007

--- 8kyu/add-property-2.js
+++ 8kyu/add-property-2.js
@@ -1,7 +1,7 @@
 function addProperty(obj, prop, value) {
     // Add your code here
     if (obj.hasOwnProperty(prop)) {
-     throw addProperty("error");
+     throw new Error ("error");
   //     console.log('Did not throw expected error');
     } else {
       obj[prop] = value;
